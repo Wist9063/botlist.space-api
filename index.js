246e17a1cabfc03f27c8144063cfbf86648093ab
Wistful__ <josharcenal@gmail.com>
2018-03-27 19:33:49 -0700

    Made index.js

---
+++ index.js
@@ -0,0 +1,95 @@
+/* Consts and stuff */
+const eURL = 'https://botlist.space/api';
+const request = require('request');
+
+function api(authorization, botID) {
+    this.auth = authorization;
+    this.id = botID;
+}
+/* Gets stats of another bot */
+api.prototype.getBot = function(id) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: eURL + '/bots/' + id,
+            headers: {
+                'User-Agent': 'botlist.space-API pkg (https://github.com/Wist9063/botlist.space-api)'
+            }
+        }, function(er, response, body) {
+            if (er) return reject(er);
+            return resolve(JSON.parse(body));
+        });
+    });
+};
+
+/* Gets entire list of bots */
+api.prototype.getWebsite = function(id) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: eURL + '/bots/',
+            headers: {
+                'User-Agent': 'botlist.space-API pkg (https://github.com/Wist9063/botlist.space-api)'
+            }
+        }, function(er, response, body) {
+            if (er) return reject(er);
+            return resolve(JSON.parse(body));
+        });
+    });
+};
+
+/* Gets a user */
+api.prototype.getUser = function(id) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: eURL + '/users/' + id,
+            headers: {
+                'User-Agent': 'botlist.space-API pkg (https://github.com/Wist9063/botlist.space-api)'
+            }
+        }, function(er, response, body) {
+            if (er) return reject(er);
+            return resolve(JSON.parse(body));
+        });
+    });
+};
+
+/* Gets Upvotes Premium Needed */
+api.prototype.getUpvotesID = function(id) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: eURL + '/bots/' + id + '?ids=true',
+            headers: {
+                'User-Agent': 'botlist.space-API pkg (https://github.com/Wist9063/botlist.space-api)',
+                "Authorization": this.auth
+            }
+        }, function(er, response, body) {
+            if (er) return reject(er);
+            if (response = '403') return reject('Premium bots only.')
+            return resolve(JSON.parse(body));
+        });
+    });
+};
+
+api.prototype.postStats = function(guildCount, shardCount) {
+    return new Promise((resolve, reject) => {
+        request.post({
+            url: eURL + '/bots/' + this.id,
+            headers: {
+                'User-Agent': 'botlist.space-API pkg (https://github.com/Wist9063/botlist.space-api)',
+                'content-type': 'application/json',
+                "Authorization": this.auth
+            },
+            body: JSON.stringify({
+                'guild_count': guildCount,
+                'shards': shardCount
+            })
+        }, function(er, response, body) {
+            if (!JSON.parse(body).error) {
+                resolve();
+            } else {
+                reject(JSON.parse(body).message);
+            }
+        });
+    })
+};
+
+
+module.exports = api;
