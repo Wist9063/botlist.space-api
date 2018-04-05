34a8bdd501839116bf3af7206063d73cdb3bcfc8
Wistful__ <josharcenal@gmail.com>
2018-04-04 18:37:22 -0700

    Added "shard support"

--- index.js
+++ index.js
@@ -3,11 +3,45 @@
         https://github.com/Wist9063/botlist.space-api
 
         Author: Wist9063 - 2018
+        Wist9063/botlist.space-api is licensed under the MIT License
+
+        ***
+
+        MIT License
+
+        Copyright (c) 2018 Wist9063
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
 */
+
 // Consts and stuff
-const eURL = 'https://botlist.space/api';
 const request = require('request');
 const snekfetch = require('snekfetch')
+
+/**
+ * Creates a new api connection.
+ * 
+ * @class botlistapi
+ * @constructor
+ * @param {String} authorization The authorization provided from the bot's token page.
+ * @param {String} botID The ID of the bot.
+ */
 
 class botlistapi {
     constructor(authorization, botID) {
@@ -21,13 +55,20 @@ class botlistapi {
         } else {
             console.log("Please enter an ID to post stats.");
         };
+        this._eURL = 'https://botlist.space/api';
     }
 
+	/**
+	 * Retrieves information about a specific bot.
+	 * @returns {Promise} The returned data.
+	 * @memberof Client
+	 * @param {String} The ID of the bot you want to get information on.
+	 */
     async getBot(id) {
         return new Promise((resolve, reject) => {
             if (!id) return reject('Please enter an ID.')
             request({
-                    url: `${eURL}/bots/${id}`,
+                    url: `${this._eURL}/bots/${id}`,
                     headers: {
                         'User-Agent': 'botlist.space-api Request PKG'
                     }
@@ -46,10 +87,16 @@ class botlistapi {
         })
     };
 
+    /**
+     * 
+     * 
+     * 
+     * 
+     */
     async getWebsite() {
         return new Promise((resolve, reject) => {
             request({
-                    url: `${eURL}/bots/`,
+                    url: `${this._eURL}/bots/`,
                     headers: {
                         'User-Agent': 'botlist.space-api Request PKG'
                     }
@@ -70,11 +117,17 @@ class botlistapi {
 
     async postStats(guild) {
         return new Promise((resolve, reject) => {
-            snekfetch.post(`${eURL}/bots/${this.id}`)
+            let data;
+            if (guild instanceof Array) {
+                data = { "shards": count };
+            } else {
+                data = { "server_count": count };
+            };
+            snekfetch.post(`${this._eURL}/bots/${this.id}`)
             .set('Content-Type', 'application/json')
             .set('Authorization', this.auth)
             .set('User-Agent', 'botlist.space-api Request PKG')
-            .send({"server_count": guild})
+            .send(data)
             .then(() => resolve('Guild Number sent'))
             .catch(err => reject(err))
         });
@@ -84,7 +137,7 @@ class botlistapi {
         return new Promise((resolve, reject) => {
             if (!id) return reject('Please enter an ID.')
             request({
-                    url: `${eURL}/user/${id}`,
+                    url: `${this._eURL}/user/${id}`,
                     headers: {
                         'User-Agent': 'botlist.space-api Request PKG'
                     }
