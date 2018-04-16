14dab0e3a830b504298b673c1c05f7fcfc595664
Wistful__ <josharcenal@gmail.com>
2018-04-15 20:47:32 -0700

    Updated test.js

--- test.js
+++ test.js
@@ -2,5 +2,6 @@ const botlistspaceapi = require('./index.js');
 const Api = new botlistspaceapi()
 
 Api.getBot('341980888239702017').then(i => { 
-    console.log(i)
+    console.log(i.name + '\nhas' + i.count + '\nguilds.')
+    console.log(`ID: ${i.id}`)
     });
