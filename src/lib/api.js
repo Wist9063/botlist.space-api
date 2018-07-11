59995bf5b08291d33eee777736c5434fc828c5d3
Wistful__ <josharcenal@gmail.com>
2018-07-11 14:59:53 -0700

    Made copyright smaller

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,35 +1,7 @@
 /**
- *       @copyright
- *       botlist.space-api written in JavaScript. (node.js)
- *       https://github.com/Wist9063/botlist.space-api
- *
- *       @author Wist9063 <https://hexaplexsoftware.ga/wist>
- *       Wist9063/botlist.space-api is licensed under the MIT License
- *
- *       ****
- *       @license Wist9063/botlist.space-api is licensed under the MIT License
- * 
- *       MIT License
- *
- *       Copyright (c) 2018 Wist9063
- *
- *       Permission is hereby granted, free of charge, to any person obtaining a copy
- *       of this software and associated documentation files (the "Software"), to deal
- *       in the Software without restriction, including without limitation the rights
- *       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *       copies of the Software, and to permit persons to whom the Software is
- *       furnished to do so, subject to the following conditions:
- *
- *       The above copyright notice and this permission notice shall be included in all
- *       copies or substantial portions of the Software.
- *
- *       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *       SOFTWARE.
+ * @file api main file
+ * @author wist9063 <https://hexaplexsoftware.ga/wist>
+ * @license MIT License
  */
 
 const nodefetch = require("node-fetch");
