bee5f9cb7df41be7e0e5f72ddbdefe66de549c96
wist9063 <josh@Joshs-iMac.local>
2018-12-31 18:12:33 -0800

    Updating test.js

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ const botlistspaceapi = require("./src/index.js");
 const Api = new botlistspaceapi.botlistAPI("xx", "341980888239702017");
 
 Api.getBot("341980888239702017").then(i => { 
-  console.log(i.name + " has " + i.count + " guilds.");
+  console.log(i.username + " has " + i.count + " guilds.");
   console.log(`ID: ${i.id}`);
 });
