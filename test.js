84b7235ec6e8e819da86ff5ca991ddaa621ec0b1
wist9063 <josh@Joshs-iMac.local>
2019-01-03 19:05:38 -0800

    Updated test.js

--- test.js
+++ test.js
@@ -2,5 +2,11 @@ const botlistspaceapi = require("./src/index.js");
 const Api = new botlistspaceapi.botlistAPI("null");
 
 Api.getBot("341980888239702017").then(i => { 
-  console.log(i);
+  console.log("Botlist: " + JSON.stringify(i));
+});
+
+const aa = new botlistspaceapi.serverlistAPI("null");
+
+aa.getServer("387812458661937152").then(i => { 
+  console.log("Serverlist: " + JSON.stringify(i));
 });
